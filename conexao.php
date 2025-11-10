4b205f850ee1bd9cbe0bb59d86654404b5c11e51
samuelhenrique1234-ops <samuel_moraes@estudante.sesisenai.org.br>
2025-11-10 08:40:41 -0300

    first commit

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "root";
+$db = "crud_system";
+
+$conn = new mysqli($host, $user, $password, $db);
+
+if ($conn->connect_error) {
+    die("Conexao Falhou: " . $conn -> connect_error);
+}
+?>
