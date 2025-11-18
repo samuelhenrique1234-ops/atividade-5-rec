65c40342dfea90ba22ec14d904f613d06185d4d7
samuelhenrique1234-ops <samuel_moraes@estudante.sesisenai.org.br>
2025-11-18 07:48:22 -0300

    ADD - cadastrar.php corrigido

--- cadastrar.php
+++ cadastrar.php
@@ -1,5 +1,5 @@
 <?php
-// Cadastro com erros de sintaxe e falta de validação
+
 include "conexao.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -7,29 +7,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
 
     $sql = "INSERT INTO user (nome, email) VALUE ('$nome', '$email')";
+    $res = mysqli_query($conn, $sql);
 
     
-    if ($conn->query($sql) === true){
-    echo "Usuário cadastrado com sucesso!";
-    } else {
-    echo "Erro" . $sql . "<br>" . $conn->error;
-    }
-       $conn -> close();
-    }
+    if ($res) {
+        echo "Usuário cadastrado com sucesso!";
+    }else
+        echo "Erro ao cadastrar!";
+}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar</title>
-</head>
-<body>
-    <form method="POST" action="cadastrar.php">
+
+<form method="POST">
     Nome: <input type="text" name="nome"><br>
     Email: <input type="email" name="email"><br>
     <input type="submit" value="Cadastrar">
 </form>
- <a href="index.php">Ver registros.</a>
-</body>
-</html>
+
+<a href='index.php'>Voltar</a>
