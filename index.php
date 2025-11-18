df7a80135b3543c6cb4f1a5031c8dd19a513012a
samuelhenrique1234-ops <samuel_moraes@estudante.sesisenai.org.br>
2025-11-18 08:36:35 -0300

    ADD - index.php correção

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php
-// Listagem com erro de lógica (ordem incorreta e falta de conexão)
 include("conexao.php");
 
-$sql = "SELECT * FROM usuarios"; // Erro de SQL: FORM ao invés de FROM
+$sql = "SELECT * FROM usuarios";
 $resultado = mysqli_query($conn, $sql);
 
 echo "<h1>Lista de Usuários</h1>";
 
 while ($linha = mysqli_fetch_array($resultado)) {
-    echo "Nome: " . $linha['nome'] . "<br>";
-    echo "Email: " . $linha['email'] . "<br><br>";
+    $id = (int)$linha['id'];
+    echo "ID: " . $id . "<br>";
+    echo "Nome: " . htmlspecialchars($linha['nome']) . "<br>";
+    echo "Email: " . htmlspecialchars($linha['email']) . "<br><br>";
+    echo "<a href='editar.php?id=" . $id . "'>Editar</a> | ";
+    echo "<a href='excluir.php?id=" . $id . "'>Excluir</a><br><br>";
+}
+
+echo "<a href='cadastrar.php'>Cadastrar novo usuário</a><br><br>";
+?>
+<?php
+if (isset($_GET['msg'])) {
+    echo "<p>" . htmlspecialchars($_GET['msg']) . "</p>";
 }
 ?>
-<a href='cadastrar.php'>Cadastrar novo</a>
